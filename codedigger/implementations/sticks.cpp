43fe6aff1ffe25cb1e2fb72a7f36e4b4c54e7d58
murtuCodeRed <murtazay619@gmail.com>
2021-10-03 13:05:09 +0400

    3OCT

---
+++ codedigger/implementations/sticks.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int x=n/2;
+    int s=n-x;
+    cout<<s<<endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
