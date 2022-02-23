edcccde6bd137fcf66303d344c83dd494a9f0c78
murtuCodeRed <murtazay619@gmail.com>
2022-02-24 01:56:03 +0400

    24FEB

---
+++ codedigger/level/1/naou.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+
+    cin>>x>>y>>z;
+
+    if(x>y+z)
+    {
+        cout<<"+";
+    }
+    else if(y>x+z)
+    {
+        cout<<"-";
+    }
+    else if(x==y && z==0)
+    {
+        cout<<0;
+    }
+    else
+    {
+        cout<<"?";
+    }
+}
---
+++ codedigger/level/1/vuecuss.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,n,m,k;
+
+    cin>>n>>m>>k;
+    if(m<n ||k<n)
+    {
+        cout<<"No"<<endl;
+    }
+    else
+    {
+        cout<<"Yes"<<endl;
+    }
+}
