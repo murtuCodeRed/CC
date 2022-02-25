03dc79b766e0ee2aa82c9a48739bb6981b05af13
murtuCodeRed <murtazay619@gmail.com>
2022-02-25 16:49:59 +0400

    25FEB

--- codedigger/level/1/nycards.cpp
+++ codedigger/level/1/nycards.cpp
@@ -4,7 +4,23 @@ using namespace std;
 int main()
 {
     string str;
+    int cvow=0;
     cin>>str;
     int l = str.length()-1;
-     
+
+    for(int i=0;i<=l;i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+        {
+            cvow++;
+        }
+        else
+        {
+        //    cout<<str[i]-'0';
+            int no=str[i]-'0';
+            if(no<10 && no%2!=0)
+                cvow++;
+        }
+    }
+    cout<<cvow;
 }
